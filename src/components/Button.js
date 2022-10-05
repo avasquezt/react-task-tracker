8d93761aef7ba7955c930f862231952050722482
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-04 22:42:01 -0500

    Add add and delete tasks functionalities

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import Proptypes from 'prop-types';
+
+const Button = ({color, text, onClick}) => {
+    return <button onClick={onClick} style={{backgroundColor: color}} className='btn'>{text}</button>
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+    text: '',
+};
+
+Button.prototype = {
+    text: Proptypes.string,
+    color: Proptypes.string,
+    onClick: Proptypes.func,
+}
+
+export default Button
